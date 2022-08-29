5f9a58f61ceffc0d93ca890d5a581a58858c2e3c
luis fonseca <kakaroto1500@gmail.com>
2022-08-29 20:16:40 -0300

    feat(product-docker):adicionando o product no container

--- apps/product/src/main.ts
+++ apps/product/src/main.ts
@@ -1,8 +1,10 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { ProductModule } from './product.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
